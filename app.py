6e97d8a2f4a019c04f6da3aff54979cf988076ff
Arham Bhatti <muhammadarham0372@gmail.com>
2026-05-30 13:53:08 +0500

    Fix PORT for Railway

--- app.py
+++ app.py
@@ -589,4 +589,5 @@ def download_file(job_id):
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
