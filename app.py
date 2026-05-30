6f36b9e1df400da0b8a70f9ed235ae79a83a1dec
Arham Bhatti <muhammadarham0372@gmail.com>
2026-05-30 13:55:20 +0500

    Fix port

--- app.py
+++ app.py
@@ -589,5 +589,5 @@ def download_file(job_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
